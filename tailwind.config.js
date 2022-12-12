a47fe2fa3d60e023d7482929d5975e4bbe4c6d1a
Berkin <103454964+berkinkinay@users.noreply.github.com>
2022-12-12 23:21:18 +0300

    Update -v2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,37 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     screens: {
-    '3xl': {'max': '21250px'},
+         '3xl': {
+            'max': '21250px'
+          },
       
-    '2xl': {'max': '1640px'},
+         '2xl': {
+            'max': '1640px'
+          },
 
-    'xl': {'max': '1279px'},
+         'xl': {
+           'max': '1279px'
+          },
 
-    'lg': {'max': '1024px'},
+         'lg': {
+           'max': '1024px'
+          },
 
-    'md': {'max': '768px'},
+         'md': {
+          'max': '768px'
+         },
 
-    'sm': {'max': '640px'},
+         'sm': {
+           'max': '640px'
+          },
 
-    'ms': {'max': '425px'},
+        'ms': {
+          'max': '425px'
+         },
 
-    'xs': {'max': '375px'},
-    },
+        'xs': {
+          'max': '375px'
+         },
     extend: {
       colors: {
         indigo: {
@@ -31,6 +46,6 @@ module.exports = {
           100: '#f2f3f5',
         },
       },
+    },
   },
- },
 }
